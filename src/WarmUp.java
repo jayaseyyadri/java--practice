9dd998fab62b5f3dac598d6e15b18f3e3bf6d8e4
jaya <jayaseyyadri@gmail.com>
2020-12-04 09:34:28 -0600

    warmup

---
+++ src/WarmUp.java
@@ -0,0 +1,31 @@
+    import java.util.Scanner;
+    public class WarmUp {
+public static void main(String [] args){
+        // TODO: The following app is broken and needs to be debugged. Change the code below to run in the expected manner.
+        //  In this exercise, assume the user will always enter a valid data type (an integer, then a string).
+        //  When working correctly, the program should have a similar output to the following:
+    /*
+        Welcome to 'PRINT THE VEGETABLE COUNT' app!
+
+        How many vegetables do you have? 3
+        What kind of vegetable? (use a singular noun, e.g. 'carrot' vs. 'carrots'): carrot
+        You have 3 carrots!
+
+
+        Process finished with exit code 0
+     */
+
+        // HINT: You can fix the unexpected behavior by altering or adding a single line
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Welcome to 'PRINT THE VEGETABLE COUNT' app!\n");
+            System.out.println("How many vegetables do you have? ");
+            int count = sc.nextInt();
+            String a=sc.nextLine();
+            System.out.print("What kind of vegetable? (use a singular noun, e.g. 'carrot' vs. 'carrots'): ");
+            String veggie = sc.nextLine();
+            System.out.printf("You have %d %ss!\n\n", count, veggie);
+
+    }
+}
+// next line takes the new line character as the input that is why we can use next
+// but that causes a problem in not taking two strings , that is why introduce a nextLine in between
--- src/first.java
+++ src/first.java
@@ -1,2 +1,33 @@
 public class first {
+    public static void main (String[]args){
+//        String name="Sai";
+//        System.out.println(name.charAt(name.length()-1));
+//        String name2="sai";
+//
+//        String j =name.toLowerCase();
+//        System.out.println(name.indexOf("s"));
+//        String k =j.replace("s","w");
+//        System.out.println(k);
+//
+//        String song="We don't need no education\nWe don't need no thought control";
+//        System.out.println(song);
+
+//String sentence="Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(sentence);
+
+//String a="In windows , the main drive is usually C:\\";
+//        System.out.println(a);
+        String b="I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslashes \\\\\\!";
+        System.out.println(b);
+
+
+    }
 }
+
+
+//    .charAt-->the char at retrieves the character at the given index
+//    .indexOf(String subString) -->the index of the string and -1 if not in the string
+//.lastIndexOf()--> same like indexOf but from starts at the end
+//.length --> length of the string
+//.replace (old char with new character)
+//String replace(String pattern, String replacement)
