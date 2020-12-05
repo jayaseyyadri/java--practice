cbeb849caa55f47eca4940bf6d1275697e453596
jaya <jayaseyyadri@gmail.com>
2020-12-05 17:43:12 -0600

    factorial

---
+++ src/factorial.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class factorial {
+
+    public static void main(String[] args) {
+        factorial();
+    }
+
+    public static void factorial() {
+        Scanner userInput = new Scanner(System.in);
+        boolean keepGoing = true;
+        int fact = 1;
+        do {
+            System.out.println("enter an integer from 1 to 10");
+             String input = userInput.next();
+               if(input.equals("no"))
+               {
+                   break;
+               }
+            int number=Integer.parseInt(input);
+                if (number < 1 || number > 10 ) {
+                    System.out.println("Enter a valid number please");
+                } else {
+                    for (int i = 1; i <= number; i++) {
+                        fact = fact * i;
+                    }
+                    System.out.println(fact);
+                    fact=1;
+
+                }
+        } while (keepGoing);
+
+    }
+}
+// whatever number i input i need to keep calculating  the multiples all the way till 1
+//  so lets say i input 2 i need 2 *1 ,3=3*2*1,4=4*3**2*1,5=5*4*3*2*1
