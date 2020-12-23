1a83c55e2bff14ed1701f9759fae0b737cc55398
jaya <jayaseyyadri@gmail.com>
2020-12-22 21:58:23 -0600

    java practice

--- src/dice.java
+++ src/dice.java
@@ -1,17 +1,18 @@
-import java.util.Random;
+
 import java.util.Scanner;
 public class dice {
 
     public static void main(String[] args) {
-
+sides();
     }
     public static void sides(){
         Scanner scanner =new Scanner(System.in);
-        Random random=new Random();
+       int random=(int)(Math.random()*2+6);
         System.out.println("enter the number of sides for a pair of dice!");
-        int sides = scanner.nextInt();
+        int sidesDice1 = scanner.nextInt();
+        int sidesDice2 = scanner.nextInt();
         System.out.println("Let's roll the dice !");
-            int numbers =1;
+        System.out.printf("you rolled %d",random );
     }
 }
 /* a dice has 6 sides max and 2 sides min
