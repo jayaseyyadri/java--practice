f0208a11edefc14ef0da15fdcafaf76ac90dc341
jaya <jayaseyyadri@gmail.com>
2020-12-22 23:34:33 -0600

    java practice

--- src/helloName.java
+++ src/helloName.java
@@ -2,17 +2,31 @@ public class helloName {
 
 //    Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
 
-    //helloName("Bob") → "Hello Bob!"
+//helloName("Bob") → "Hello Bob!"
 //helloName("Alice") → "Hello Alice!"
 //helloName("X") → "Hello X!"
-    public String sayName(String name) {
-        return "Hello" + " " + name +"!";
-    }
 
+
+//    public String sayName(String name) {
+//        return "Hello" + " " + name +"!";
+//    }
+
+
+//    Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+
+//    [DISCUSS UNDERSTANDING]
+
+//public String joinStrings(String out, String word){
+//        return (out.substring(0,2)+ word + out.substring(5));
+//}
+
+
+//Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". Note that str.length() returns the length of a string.
 
     public static void main(String[] args) {
          helloName name= new helloName();
-        System.out.println(name.sayName("cess"));
 
+//        System.out.println(name.joinStrings("<<","saik"));
+//        System.out.println(name.sayName);
     }
 }
