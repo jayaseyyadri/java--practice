5af02b8f9ec4bd448e85515aa3771556c8cd46e2
jaya <jayaseyyadri@gmail.com>
2020-12-24 22:40:37 -0600

    java exercises:

--- src/helloName.java
+++ src/helloName.java
@@ -40,6 +40,14 @@ helloName("Bob") → "Hello Bob!"
 //        return a+b;
 //    }
 //}
+/**Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2.*/
+
+                /*SOLUTION*/
+
+//public String middleTwo(String str) {
+//    return str.substring(str.length()/2-1,str.length()/2+1);
+//}
+
 
     public static void main(String[] args) {
          helloName name= new helloName();
